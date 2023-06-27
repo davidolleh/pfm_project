c49291b42eabb6284433d54067547695c5e968d7
hwang seung joon <asherolleh@gmail.com>
2023-06-27 13:00:08 +0900

    <FEAT> add project setting

---
+++ src/main/kotlin/com/example/pfm_project/PfmProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pfm_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PfmProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<PfmProjectApplication>(*args)
+}
